3357679783788fb24c9626ced4178b9b7f07d6b2
1165sp <21165@students.mrgs.school.nz>
2024-05-08 11:54:36 +1200

    Added Quiz Question #comment and list component for questions

---
+++ quiz_questions.py
@@ -0,0 +1,32 @@
+# Quiz Questions Components 
+
+quiz_questions = [
+    {
+
+       "question": "(One Piece) - Where was the character Luffy born?",
+        "options": ["Shimotsuki Village", "Syrup Village", "Foosha Village", "Arlong Park"],
+        "answer": "Foosha Village"
+    },
+    {
+       "question": "(My Hero Acadamia) - What is All Might's actual name?",
+        "options": ["All Might", "Midoriya", "Baku", "Toshinori Yagi"],
+        "answer":  "Toshinori Yagi"
+    },
+    {
+       "question": "(Naruto) - Who said this quote? \"I had no fear of defeat right from the start of this match, I believe it!\" ", 
+        "options": ["Kakashi", "Naruto", "Tsunade", "Minato"],
+        "answer": "Naruto"
+    },
+    {
+       "question": "(One Piece) - Which character gifted Luffy his Straw hat?", 
+        "options": ["Usopp", "Brook", "Shanks", "Kin'emon"],
+        "answer": "Shanks"
+    },
+    {
+       "question": "(One Piece) - Which character gifted Luffy his Straw hat?", 
+        "options": ["Usopp", "Brook", "Shanks", "Kin'emon"],
+        "answer": "Shanks"
+    },
+  
+  
+]

73c6ac8eea76a8a5b2779e762ed9c441d7165281
1165sp <21165@students.mrgs.school.nz>
2024-05-09 10:38:58 +1200

    Completely Revised Code, Added canvas for easier wdiget entries

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from tkinter import*
+import tkinter as tk
+
+import random
+
+names = []
+global questions_answers
+asked = [] 
+score = 0
+
+root = tk.Tk()
+root.title("Anime Quiz")
+
+Welcome = tk.Canvas(root, width=500, height=500)
+Welcome.pack()
+
+
+root.mainloop()

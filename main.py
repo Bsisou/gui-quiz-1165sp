25eb12eb87435c1af8e4cf2af2bf35e6c727c933
1165sp <21165@students.mrgs.school.nz>
2024-05-09 10:48:53 +1200

    Added Title text (Label) and added name entry box (Store Name)

--- main.py
+++ main.py
@@ -13,5 +13,15 @@ root.title("Anime Quiz")
 Welcome = tk.Canvas(root, width=500, height=500)
 Welcome.pack()
 
+label1 = tk.Label(root, text="The Glorious Anime Quiz!")
+label1.config(font=("courier new", 20))
+Welcome.create_window(200, 35, window=label1)
+
+label2 = tk.Label(root, text="Enter Your Name Here")
+label2.config(font=("courier new", 10))
+Welcome.create_window(200, 100, window=label2)
+
+entry = tk.Entry(root)
+Welcome.create_window(200, 140, window=entry)
 
 root.mainloop()

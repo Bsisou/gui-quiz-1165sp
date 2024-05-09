5b1141d50c52d68d3eaf6342aafc8c6e96cbb1b7
1165sp <21165@students.mrgs.school.nz>
2024-05-10 09:42:53 +1200

    Added Quiz Class and pillow module for image background (Window Not working at the moment)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tkinter as tk
+import tkinter as tk #importing tkinter
 
-import random
+import random #importing random module
+
+import PIL #importing Python Image library Module
+
+from PIL import Image, ImageTk #importing Image and ImageTk from Pillow
+
 
 names = []
 global questions_answers
@@ -10,18 +15,24 @@ score = 0
 root = tk.Tk()
 root.title("Anime Quiz")
 
-Welcome = tk.Canvas(root, width=500, height=500)
-Welcome.pack()
+class Quiz:
+    def __init__(Welcome, parent):
+        Welcome.quiz_frame=Frame(parent, bg = '#FFAF45')
+        Welcome.quiz_frame.grid()    
 
-label1 = tk.Label(root, text="The Glorious Anime Quiz!")
-label1.config(font=("courier new", 20))
-Welcome.create_window(200, 35, window=label1)
+        Welcome = tk.Canvas(root, width=640, height=480)
+        Welcome.configure(bg='#FFAF45')
 
-label2 = tk.Label(root, text="Enter Your Name Here")
-label2.config(font=("courier new", 10))
-Welcome.create_window(200, 100, window=label2)
+        label1 = tk.Label(root, text="The Glorious Anime Quiz!")
+        label1.config(font=("courier new", 20), bg=('#FFAF45'))
+        Welcome.create_window(200, 35, window=label1)
 
-entry = tk.Entry(root)
-Welcome.create_window(200, 140, window=entry)
+        label2 = tk.Label(root, text="Enter Your Name Here")
+        label2.config(font=("courier new", 10), bg=('#FFAF45'))
+        Welcome.create_window(200, 100, window=label2)
+
+        entry = tk.Entry(root)
+        Welcome.create_window(200, 140, window=entry)
+        Welcome.pack()
 
 root.mainloop()

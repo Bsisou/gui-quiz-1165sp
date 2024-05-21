3e834d298a10a94cc23fd14a5e72d027c987b216
1165sp <21165@students.mrgs.school.nz>
2024-05-22 11:59:00 +1200

    Removed duplicate code, general cleaning of entire file

--- main.py
+++ main.py
@@ -10,48 +10,50 @@ asked=[]
 score=0
 
 class Quizstarter: 
-  def __init__(self, parent): 
+  def __init__(main, parent): 
     background_color="OldLace"
-    self.quiz_frame=Frame(parent, bg = "grey", padx=100, pady=100) #"The Frame"
-    self.quiz_frame.grid()
-    self.bg_image = Image.open("Image1.png") # V background image V
-    self.bgimage = self.bg_image.resize((350, 500), Image.LANCZOS)
-    self.bg_image = ImageTk.PhotoImage(self.bg_image)
-    self.bg_image = PhotoImage(file="Image1.png")
+    main.quiz_frame=Frame(parent, padx=100, pady=100) #"The Frame"
+    main.quiz_frame.grid()
+    main.bg_image = Image.open("Image1.png") # Background image
+    main.bgimage = main.bg_image.resize((350, 500), Image.LANCZOS)
+    main.bg_image = ImageTk.PhotoImage(main.bg_image)
+    main.bg_image = PhotoImage(file="Image1.png")
 
-    self.image_label = Label(self.quiz_frame, image=self.bg_image) # Label for background image
-    self.image_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-    self.heading_label=Label(self.quiz_frame, text="The Glorious Anime Quiz!", bg="orange") #Title of quiz
-    self.heading_label.grid(row=0, padx=20)
+    main.image_label = Label(main.quiz_frame, image=main.bg_image) # Label for background image
+    main.image_label.place(x=0, y=0, relwidth=1, relheight=1)
+   
+    main.heading_label=Label(main.quiz_frame, text="The Glorious Anime Quiz!", bg="orange") #Title of quiz
+    main.heading_label.grid(row=0, padx=20)
 
     #Entry Box Label
-    self.user_label=Label(self.quiz_frame, text="Enter your name: ", bg="red")
-    self.user_label.grid(row=1, padx=20, pady=20)
+    main.user_label=Label(main.quiz_frame, text="Enter your name: ", bg="red") 
+    main.user_label.grid(row=1, padx=20, pady=20)
 
     #The Entry Box
-    self.entry_box=Entry(self.quiz_frame) 
-    self.entry_box.grid(row=2, padx=20, pady=20)
+    main.entry_box=Entry(main.quiz_frame) 
+    main.entry_box.grid(row=2, padx=20, pady=20)
 
     #The Continue Button
-    self.continue_button = Button(self.quiz_frame, text="Continue", bg="yellow")
-    self.continue_button.grid(row=3, padx=20, pady=20)
+    main.continue_button = Button(main.quiz_frame, text="Continue", bg="yellow")
+    main.continue_button.grid(row=3, padx=20, pady=20)
 
 if __name__ == "__main__":
-  root = Tk()
-  root.title("Quiz :)")
-  root.geometry("350x500")
-  quiz_instance = Quizstarter(root)
-  quiz_starter_object = Quizstarter(root) #instantiation, making an instance of the class Quiz
+  WelcomeW = Tk()
+  WelcomeW.title("Anime Quiz :)")
+  WelcomeW.geometry("350x500")
+  quiz_instance = Quizstarter(WelcomeW)
+  quiz_starter_object = Quizstarter(WelcomeW) #instantiation, making an instance of the class Quiz
+  WelcomeW.mainloop
+  
 
-  def name_collection(self):
-    name=self.entry_box.get()
+  def name_collection(main):
+    name=main.entry_box.get()
     name.append(name) 
-    self.quiz_frame.destroy()
-    Quiz(root)
+    main.quiz_frame.destroy()
+    Quiz(WelcomeW)
   
   # So the window doesn't disappear
-  root.mainloop() 
+WelcomeW.mainloop() 
 
 def randomiser():
   global qnum
@@ -60,25 +62,4 @@ def randomiser():
     asked.append(qnum)
   elif qnum in asked:
     randomiser()
-
-randomiser()
-
-randomiser()
-if __name__ == "__main__":
-  root = Tk()
-  root.title("Anime Quiz!")
-  quiz_instance = Quizstarter(root)
-  root.mainloop
-
-
-    
-
-
-
-
-
-
-
-
-
 

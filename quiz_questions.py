def882ef4b4d8a081ed7961db5f88449cf6e6bfb
1165sp <21165@students.mrgs.school.nz>
2024-06-28 10:25:52 +1200

    added 19 more questions to quiz_questions as per user feedback

--- quiz_questions.py
+++ quiz_questions.py
@@ -27,10 +27,104 @@ quiz_questions = [
         "options": ["All Might", "Mount Lady", "Shigaraki", "Baku"],
         "answer": "All Might"
     },
+     {
+        "question": "(Naruto) - Who is known as the Copy Ninja?",
+        "options": ["Kakashi", "Naruto", "Sasuke", "Itachi"],
+        "answer": "Kakashi"
+    },
+    {
+        "question": "(One Piece) - What is the name of Zoro's sword style?",
+        "options": ["Haki", "Rokushiki", "Nitoryu", "Santoryu"],
+        "answer": "Santoryu"
+    },
+    {
+        "question": "(My Hero Academia) - Who is the principal of U.A. High School?",
+        "options": ["Endeavor", "Present Mic", "Nezu", "Aizawa"],
+        "answer": "Nezu"
+    },
+    {
+        "question": "(Naruto) - What is the name of Naruto's son?",
+        "options": ["Boruto", "Sarada", "Mitsuki", "Konohamaru"],
+        "answer": "Boruto"
+    },
+    {
+        "question": "(One Piece) - What is the name of Sanji's dream All Blue?",
+        "options": ["The Grand Line", "East Blue", "North Blue", "All Blue"],
+        "answer": "All Blue"
+    }, 
       {
-       "question": "(One Piece) - Which character gifted Luffy his Straw hat?", 
-        "options": ["Usopp", "Brook", "Shanks", "Kin'emon"],
-        "answer": "Shanks"
-    }
-
+        "question": "(One Piece) - What is the name of Sanji's dream All Blue?",
+        "options": ["The Grand Line", "East Blue", "North Blue", "All Blue"],
+        "answer": "All Blue"
+    },
+    {
+        "question": "(My Hero Academia) - What is Bakugo's hero name?",
+        "options": ["Dynamight", "Kacchan", "Red Riot", "Shoto"],
+        "answer": "Kacchan"
+    },
+    {
+        "question": "(Naruto) - Who is Naruto's first teacher at the Ninja Academy?",
+        "options": ["Kakashi", "Iruka", "Jiraiya", "Hiruzen"],
+        "answer": "Iruka"
+    },
+    {
+        "question": "(One Piece) - What is the name of the main antagonist pirate group?",
+        "options": ["Red Hair Pirates", "Blackbeard Pirates", "Straw Hat Pirates", "Big Mom Pirates"],
+        "answer": "Blackbeard Pirates"
+    },
+    {
+        "question": "(My Hero Academia) - Who is Todoroki's father?",
+        "options": ["Endeavor", "Present Mic", "All Might", "Aizawa"],
+        "answer": "Endeavor"
+    },
+    {
+        "question": "(Naruto) - Who is known as the Legendary Sannin?",
+        "options": ["Jiraiya", "Orochimaru", "Tsunade", "All of the above"],
+        "answer": "All of the above"
+    },
+    {
+        "question": "(One Piece) - What is the name of the main antagonist organization?",
+        "options": ["Marines", "Revolutionary Army", "Cipher Pol", "Baroque Works"],
+        "answer": "Marines"
+    },
+    {
+        "question": "(My Hero Academia) - Who is the class president of Class 1-A?",
+        "options": ["Iida", "Tokoyami", "Momo", "Bakugo"],
+        "answer": "Iida"
+    },
+    {
+        "question": "(Naruto) - What clan does Sasuke belong to?",
+        "options": ["Uchiha", "Hyuga", "Senju", "Nara"],
+        "answer": "Uchiha"
+    },
+    {
+        "question": "(One Piece) - What is the name of the island where Luffy trained during the timeskip?",
+        "options": ["Wano", "Dressrosa", "Punk Hazard", "Rusukaina"],
+        "answer": "Rusukaina"
+    },
+    {
+        "question": "(My Hero Academia) - Who is the No. 1 hero currently?",
+        "options": ["All Might", "Endeavor", "Hawks", "Best Jeanist"],
+        "answer": "Hawks"
+    },
+    {
+        "question": "(Naruto) - Who was the Fourth Hokage?",
+        "options": ["Tsunade", "Minato", "Hiruzen", "Kakashi"],
+        "answer": "Minato"
+    },
+    {
+        "question": "(One Piece) - What is the name of Nami's home village?",
+        "options": ["Kokoyashi Village", "Loguetown", "Orange Town", "Ohara"],
+        "answer": "Kokoyashi Village"
+    },
+    {
+        "question": "(My Hero Academia) - Who is Deku's childhood friend?",
+        "options": ["Ochaco Uraraka", "Momo Yaoyorozu", "Tsuyu Asui", "Tenya Iida"],
+        "answer": "Ochaco Uraraka"
+    },
+    {
+        "question": "(Naruto) - What is the name of Gaara's sand-based jutsu?",
+        "options": ["Sandstorm Coffin", "Quicksand Waterfall Flow", "Sand Waterfall Funeral", "Sand Coffin"],
+        "answer": "Sand Coffin"
+    },
 ]
